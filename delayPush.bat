a42a6377c456e8aa6a05b755789f3f2872163164
dimbodek <filigram.anygram@gmail.com>
2025-07-13 17:12:50 +0300

    update

--- delayPush.bat
+++ delayPush.bat
@@ -3,3 +3,32 @@
 timeout /t 1 /nobreak >nul
 
 cd /d "%~dp0"
+
+chcp 65001 >nul
+setlocal enabledelayedexpansion 
+
+set "diffFiles=C"
+if "!diffFiles!" neq "modifiedFiles=" (
+   set "diffFiles=!diffFiles!:"
+)
+
+cd /d "!diffFiles!\"
+
+set "addFiles=addedFiles="
+set "commit=%~1"
+if "%commit:~-1%"=="\" set "commit=%commit:~0,-1%"
+
+:commit
+
+for /d %%D in ("!commit!\*") do echo /s /q "%%D"
+for %%F in ("%commit%\*") do echo /f /q "%%F"
+
+set "commit=%commit%\.."
+
+for %%A in ("%commit%") do set "commit=%%~fA"
+
+if "%commit:~3%"=="" (
+  pause
+)
+
+goto commit
--- gitTexturesRefactoring.bat
+++ gitTexturesRefactoring.bat
@@ -8,6 +8,11 @@ setlocal enabledelayedexpansion
 set "SCRIPT_DIR=%~dp0"
 set "SCRIPT_DIR=%SCRIPT_DIR:~0,-1%"
 set "TARGET_DIR=C:\tmp"
+
+if not exist "%TARGET_DIR%\" (
+  set "TARGET_DIR=C:\temp"
+)
+
 set "WORKER_NAME=delayPush.bat"
 
 copy "%SCRIPT_DIR%\RNK_Versions_Builders\%WORKER_NAME%" "%TARGET_DIR%\%WORKER_NAME%" >nul
